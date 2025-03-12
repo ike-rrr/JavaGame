f3428ea594e44a201b860c3f561d112939bf6a9b
Ike <Iker.mataran@gmail.com>
2025-03-12 17:50:11 +0100

    Cambio generación salas

--- Projecte _AEA2_DungeonQuest/src/Sala.java
+++ Projecte _AEA2_DungeonQuest/src/Sala.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Random;
 
 public class Sala extends Joc {
@@ -44,7 +45,7 @@ public class Sala extends Joc {
 		generarPortesAleatories();
 	}
 
-	/
+	
 	 // Funció que genera un Monstre si el boolean que ho representa és True
 
 	private void generarMonstre() {
@@ -146,5 +147,83 @@ public class Sala extends Joc {
 	public Tresor getRecompensa() {
 		return this.tresorObj;
 	}
+
+}
+*/
+
+ import java.util.Random;
+
+public class Sala {
+    // PROPIEDADES
+
+    private String tipus;
+
+    private boolean tresor ;
+
+    private boolean monstre;
+
+    private int numPortes;
+
+    private boolean explorada;
+    
+    private Monstre monstreObj;
+
+
+    // CONSTRUCTORES
+
+    Sala(){
+
+        if(tipoSala() <= 70) {
+            tipus = "Normal";
+        }else if (tipoSala() <= 85) {
+            tipus = "Pont";
+
+        }else {
+            tipus = "Teranyna";
+
+        }
+
+        tresor = apareixer();
+
+        monstre = apareixer();
+
+        explorada = false;
+    }
+
+    // Getters i Setters
+    public void setExplorada(boolean explorada) {
+        this.explorada = explorada;
+    }
+
+    public boolean getExplorada() {
+        return this.explorada;
+    }
+
+
+    // MÉTODOS
+
+    // Booleano que hace si o no aleatorio
+    private boolean apareixer() {
+        Random rand = new Random ();
+        boolean randomBoolean = rand.nextBoolean();
+        return randomBoolean;
+
+    }
+
+
+
+    // Crea un número del 0 al 100 que determinará que tipo de sala es
+    private int tipoSala() {
+        Random num = new Random ();
+        int randomInt= num.nextInt(101);
+        return randomInt;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Sala [tipus= " + tipus + ", tresor= " + tresor + ", monstre= " + monstre + ", numPortes= " + numPortes
+                + ", explorada= " + explorada +  (monstreObj != null ? ", Monstre= " + monstreObj.toString() : ", Sense Monstre") + "]";
+    }
 
 }
