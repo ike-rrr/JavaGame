1b1233566eda1463d36eff600c6f6486cdcae9be
i <iker.mataran@gmail.com>
2025-04-24 17:15:45 +0200

    Cambios a lo de posición

--- Projecte _AEA2_DungeonQuest/src/Personatge.java
+++ Projecte _AEA2_DungeonQuest/src/Personatge.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.util.Arrays;
 
 public class Personatge extends Joc {
@@ -8,33 +7,42 @@ public class Personatge extends Joc {
     private int experiencia;
     private int agilitat;
     private int forsa;
-    private Point posicio = new Point();
+    private int posicioX; // Coordenada X
+    private int posicioY; // Coordenada Y
     private Tresor[] equipament;
+    private static final int MAZMORRA_SIZE = 10; // Tamaño de la mazmorra (10x10)
 
     public Personatge(String nom) {
         this.nom = nom;
         generarValorsPersonatge();
     }
 
-    public static Point getPosicio(Personatge personatge) {
-        return personatge.posicio;
+    public int getPosicioX() {
+        return posicioX;
     }
-    
+
+    public int getPosicioY() {
+        return posicioY;
+    }
+
     private void setPosicio(int x, int y) {
-        this.posicio.x = x;
-        this.posicio.y = y;
+        // Asegurar que las coordenadas estén dentro de los límites de la mazmorra
+        if (x >= 0 && x < MAZMORRA_SIZE && y >= 0 && y < MAZMORRA_SIZE) {
+            this.posicioX = x;
+            this.posicioY = y;
+        }
     }
 
     public void utilitzarTresor(int i) {
         if (i < 0 || i >= equipament.length || equipament[i] == null) return;
-        
+
         Tresor objTresor = this.equipament[i];
         int valor = objTresor.getValor();
         boolean usTresor = objTresor.getUsTresor();
-        
-        if(usTresor) {
-            if(valor >= 100){
-                this.setPosicio(generarValorAleatori(0, 10), generarValorAleatori(0, 10));
+
+        if (usTresor) {
+            if (valor >= 100) {
+                this.setPosicio(generarValorAleatori(0, MAZMORRA_SIZE - 1), generarValorAleatori(0, MAZMORRA_SIZE - 1));
                 objTresor.setValor(valor - 100);
             } else {
                 System.out.println("No hi ha fum suficient");
@@ -43,16 +51,16 @@ public class Personatge extends Joc {
             System.out.println("Aquest tresor no té us");
         }
     }
-    
+
     public void explorarSala() {
-        // Implementación básica - debería interactuar con la sala actual
         System.out.println("Explorant la sala actual...");
     }
 
     public void generarValorsPersonatge() {
         this.vida = generarValorAleatori(5, 20);
         this.atac = generarValorAleatori(1, 4);
-        this.posicio.setLocation(0, 0); // Empieza en (0,0)
+        this.posicioX = 0; // Empieza en (0, 0)
+        this.posicioY = 0;
         this.experiencia = 0;
         this.agilitat = generarValorAleatori(4, 11);
         this.forsa = generarValorAleatori(4, 11);
@@ -60,28 +68,39 @@ public class Personatge extends Joc {
     }
 
     public void mourePersonatge(char direccio) {
+        int newX = posicioX;
+        int newY = posicioY;
+
         switch (direccio) {
             case 'N':
-                if (posicio.x > 0) posicio.x--;
+                newX--;
                 break;
             case 'S':
-                posicio.x++;
+                newX++;
                 break;
             case 'E':
-                posicio.y++;
+                newY++;
                 break;
             case 'O':
-                if (posicio.y > 0) posicio.y--;
+                newY--;
                 break;
             default:
                 throw new IllegalArgumentException("Direcció no vàlida: " + direccio);
+        }
+
+        // Verificar límites antes de actualizar la posición
+        if (newX >= 0 && newX < MAZMORRA_SIZE && newY >= 0 && newY < MAZMORRA_SIZE) {
+            posicioX = newX;
+            posicioY = newY;
+        } else {
+            System.out.println("No es pot sortir de la mazmorra!");
         }
     }
 
     @Override
     public String toString() {
         return "[nom=" + nom + ", vida=" + vida + ", atac=" + atac + ", experiencia=" + experiencia +
-                ", agilitat=" + agilitat + ", forsa=" + forsa + ", posició=" + posicio + 
+                ", agilitat=" + agilitat + ", forsa=" + forsa + ", posició=(" + posicioX + ", " + posicioY + ")" +
                 ", equipament=" + Arrays.toString(equipament) + "]";
     }
 }
